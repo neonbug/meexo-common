3727f295398850b0814d32ffe5201760bf237a63
Marko Zagar <marko.zagar@gmail.com>
2016-02-28 00:34:31 +0100

    kcfinder added

--- src/assets/admin_assets/js/ckeditor/config.js
+++ src/assets/admin_assets/js/ckeditor/config.js
@@ -7,4 +7,11 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	// 
+	config.filebrowserBrowseUrl = '../../../vendor/common/admin_assets/js/ckeditor/plugins/kcfinder/browse.php?opener=ckeditor&type=files';
+	config.filebrowserImageBrowseUrl = '../../../vendor/common/admin_assets/js/ckeditor/plugins/kcfinder/browse.php?opener=ckeditor&type=images';
+	config.filebrowserFlashBrowseUrl = '../../../vendor/common/admin_assets/js/ckeditor/plugins/kcfinder/browse.php?opener=ckeditor&type=flash';
+	config.filebrowserUploadUrl = '../../../vendor/common/admin_assets/js/ckeditor/plugins/kcfinder/upload.php?opener=ckeditor&type=files';
+	config.filebrowserImageUploadUrl = '../../../vendor/common/admin_assets/js/ckeditor/plugins/kcfinder/upload.php?opener=ckeditor&type=images';
+	config.filebrowserFlashUploadUrl = '../../../vendor/common/admin_assets/js/ckeditor/plugins/kcfinder/upload.php?opener=ckeditor&type=flash';
 };
