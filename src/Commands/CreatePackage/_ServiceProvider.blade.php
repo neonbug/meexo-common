03084ef9c2d9dbc015f045eb799d664d66e413f4
Tadej Kanižar <tadej@ncode.si>
2016-02-29 15:14:07 +0100

    Add admin menu icon to CreateCommand template, to make it clearer where to change that icon

--- src/Commands/CreatePackage/_ServiceProvider.blade.php
+++ src/Commands/CreatePackage/_ServiceProvider.blade.php
@@ -84,8 +84,9 @@ class ServiceProvider extends \Neonbug\Common\Providers\BaseServiceProvider {
 		});
 		
 		//admin
-		$router->group([ 'prefix' => $admin_locale . '/admin/' . static::PREFIX, 'middleware' => [ 'auth.admin', 'admin.menu' ], 
-			'role' => static::ROLE ], function($router)
+		$router->group([ 'prefix' => $admin_locale . '/admin/' . static::PREFIX, 
+			'middleware' => [ 'auth.admin', 'admin.menu' ], 'role' => static::ROLE, 
+			'menu.icon' => 'arrow right' ], function($router)
 		{
 			$router->get('list', [
 				'as'   => static::PREFIX . '::admin::list', 
