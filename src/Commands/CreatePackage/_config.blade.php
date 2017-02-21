0b1f54d083d48165fa3c097555633315ffa58c7a
Tadej Kanižar <tadej@ncode.si>
2017-02-21 17:37:50 +0100

    Add slug_routes_at_root to newly created package configs

--- src/Commands/CreatePackage/_config.blade.php
+++ src/Commands/CreatePackage/_config.blade.php
@@ -4,6 +4,28 @@ return [
 	'model' => '\{{ $namespace }}\{{ $package_name }}\Models\{{ $model_name }}', 
 	'supports_preview' => true, 
 	
+	/*
+	|--------------------------------------------------------------------------
+	| Slug routes at root
+	|--------------------------------------------------------------------------
+	|
+	| Set this to TRUE, if you want routes with slugs to be accessible from 
+	| root as well.
+	|
+	| Example: an item with slug 'abc' is accessible by route /en/content/abc
+	| if slug_routes_at_root is set to TRUE, then it is also accessible by 
+	| route /en/abc.
+	|
+	| Warning: setting this to TRUE will cause named routes 
+	| (e.g. text::slug::abc) to route to root slugs (e.g. /en/abc), instead 
+	| of the default ones (e.g. /en/content/abc).
+	| Default routes are then accessible using alternative name, where slug 
+	| is replaced by slug-default (e.g. text::slug-default::abc).
+	|
+	*/
+	
+	'slug_routes_at_root' => false, 
+	
 	'list' => [
 		'fields' => [
 			'{{ 'id_' . str_replace('\\', '', snake_case($model_name)) }}' => [
