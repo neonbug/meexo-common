726f64c4fcb23cfc4aaa9736c4ee2c6399df4521
Tadej Kanižar <tadej@ncode.si>
2018-06-04 12:47:00 +0200

    Fix error when trying to display link to an item without a slug

--- src/resources/admin_views/list_fields/content-link.blade.php
+++ src/resources/admin_views/list_fields/content-link.blade.php
@@ -1,1 +1,5 @@
-<a href="{{ route($route_prefix . '::slug::' . $item->slug) }}">{{ trans('common::admin.list.content-link') }}</a>
+@if (mb_strlen(trim($item->slug)) == 0)
+	<span>/</span>
+@else
+	<a href="{{ route($route_prefix . '::slug::' . $item->slug) }}">{{ trans('common::admin.list.content-link') }}</a>
+@endif
