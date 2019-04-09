fe3d98e3efc21b4222fb265130208f121ef86b1d
Tadej Kanižar <tadej@ncode.si>
2019-04-09 12:09:25 +0200

    Improve MySQL compatibility

--- src/resources/admin_views/list_fields/boolean.blade.php
+++ src/resources/admin_views/list_fields/boolean.blade.php
@@ -1,4 +1,4 @@
-@if ($item->$field_name === true)
+@if ($item->$field_name === true || $item->$field_name === 1)
 	<i class="large green checkmark icon"></i>
 @else
 	<i class="large red close icon"></i>
