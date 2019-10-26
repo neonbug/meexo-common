ba22e6157809dfef9ffb7cb4c5385a704ec0de05
Tadej Kanižar <tadej@ncode.si>
2019-10-26 22:00:10 +0200

    Add support for configurable image width

--- src/resources/admin_views/list_fields/image.blade.php
+++ src/resources/admin_views/list_fields/image.blade.php
@@ -1,3 +1,6 @@
 @if ($item->$field_name != null && $item->$field_name != '')
-	<img src="{!! Croppa::url_resize('uploads/' . $route_prefix . '/' . $item->$field_name, 180) !!}" />
+	<img src="{!! Croppa::url_resize(
+		'uploads/' . $route_prefix . '/' . $item->$field_name,
+		array_key_exists('width', $field) ? $field['width'] : 180
+	) !!}" />
 @endif
