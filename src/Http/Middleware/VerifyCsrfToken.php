309d03b9352a53dd024148e90cbb829b1bb79404
Tadej Kanižar <tadej@ncode.si>
2020-04-26 22:00:25 +0200

    Update to Laravel 5.7

--- src/Http/Middleware/VerifyCsrfToken.php
+++ src/Http/Middleware/VerifyCsrfToken.php
@@ -6,7 +6,23 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 use Illuminate\Session\TokenMismatchException;
 
 class VerifyCsrfToken extends Middleware {
+	
+	/**
+	 * Indicates whether the XSRF-TOKEN cookie should be set on the response.
+	 *
+	 * @var bool
+	 */
+	protected $addHttpCookie = true;
 
+	/**
+	 * The URIs that should be excluded from CSRF verification.
+	 *
+	 * @var array
+	 */
+	protected $except = [
+		//
+	];
+	
 	/**
 	 * Handle an incoming request.
 	 *
