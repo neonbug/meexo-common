adc46bd2e74e94ade387d2fb50299d0b6a7c29fc
Tadej Kanižar <tadej@ncode.si>
2023-09-20 12:10:34 +0200

    Add note support to multiline text field

--- src/resources/admin_views/add_fields/multi_line_text.blade.php
+++ src/resources/admin_views/add_fields/multi_line_text.blade.php
@@ -6,6 +6,10 @@
 		<div class="field">
 			<textarea name="field[{{ $id_language }}][{{ $field['name'] }}]" 
 				data-name="{{ $field['name'] }}" data-type="multi_line_text">{{ $field['value'] }}</textarea>
+			
+			@if (array_key_exists('note', $field) && $field['note'] != '')
+				<div><div class="ui pointing label">{!! trans($field['note']) !!}</div></div>
+			@endif
 		</div>
 	</td>
 </tr>
